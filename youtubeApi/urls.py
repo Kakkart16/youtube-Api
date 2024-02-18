65efaa4c784ea991b74fee2c2b7859765efc207c
Kakkart16 <kakkartushar.16@gmail.com>
2024-02-18 14:42:55 +0530

    Created Serializers and VideoList View

--- youtubeApi/urls.py
+++ youtubeApi/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path,include 
+from django.urls import path, include 
+from .views import VideoList
 
 urlpatterns = [
-    
+    path('videos/', VideoList.as_view(), name='video-list'),
 ]
--- youtubeApi/views.py
+++ youtubeApi/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Video
+from .serializers import VideoSerializer
+from rest_framework.pagination import PageNumberPagination
 
+class VideoPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    
 # Create your views here.
+class VideoList(ListAPIView):
+    queryset = Video.objects.all().order_by('-published_datetime')
+    serializer_class = VideoSerializer
+    pagination_class = VideoPagination 
+
+
