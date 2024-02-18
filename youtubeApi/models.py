ea1738958c2679fbee2addef744376689aafd35b
Kakkart16 <kakkartushar.16@gmail.com>
2024-02-18 13:32:07 +0530

    Created and Registered Video Model

--- youtubeApi/models.py
+++ youtubeApi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    published_datetime = models.DateTimeField()
+    thumbnail_url = models.URLField()
+
+    def __str__(self):
+        return self.title
